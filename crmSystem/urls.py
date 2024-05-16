d56924d75030d699cbb360331ea2aa4c827a7fa5
unawarexi <unawarexi@gmail.com>
2024-05-16 03:08:55 +0100

    configuring Lead Views for landingPage

--- crmSystem/urls.py
+++ crmSystem/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from Leads.views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", landing_page, name = "landing_page"),
     path("leads/", include("Leads.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
