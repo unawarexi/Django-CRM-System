79885bc4b248e2c559e390c9e502c8318dd13149
unawarexi <unawarexi@gmail.com>
2024-05-16 02:01:52 +0100

    Routing URL, unique page ID,  Router names

--- Leads/urls.py
+++ Leads/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 
-app_name = "leads"
+# app_name = "leads"
 
 urlpatterns = [
     path("", views.lead_List, name = "leads"),
-    path("<int:pk>/", views.lead_detail, name = "leadDetail"),
+    path("<int:pk>/", views.lead_detail, name = "lead_detail"),
     path("<int:pk>/update", views.lead_update, name = "lead_update"),
-    path("create/", views.lead_create, name = "leadCreate"),
+    path("<int:pk>/delete", views.lead_delete, name = "lead_delete"),
+    path("create/", views.lead_create, name = "lead_create"),
     
 ]
--- Leads/views.py
+++ Leads/views.py
@@ -37,34 +37,74 @@ def lead_create(request):
 
 
 def lead_update(request, pk):
-    lead = lead.object.get(id=pk)
+    lead = Leads.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if (request.method == "POST"):
+         form = LeadModelForm(request.POST, instance=lead)
+         if form.is_valid():
+            form.save()
+            return redirect("/leads")
+            
     context = {
-        "lead": lead
-        
+        "lead": lead,
+        "form": form
     }
     return render(request, "leads/lead_update.html", context)
 
-# def lead_create(request):
-#     form =LeadModelForm()
+def lead_delete(request, pk):
+    lead = Leads.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
+   
+
+
+# def lead_update(request, pk):
+#     lead = Leads.objects.get(id=pk)
+#     form =LeadForm()
 #     if (request.method == "POST"):
-#         form = LeadModelForm(request.POST)
+#         form = LeadForm(request.POST)
 #         if form.is_valid():
-#             print("form is valid")
 #             print(form.cleaned_data)
 #             first_name = form.cleaned_data["first_name"]
 #             last_name = form.cleaned_data["last_name"]
 #             age = form.cleaned_data["age"]
-#             agent = Agent.objects.first()
-#             Leads.objects.create(
-#                 first_name = first_name, 
-#                 last_name = last_name, 
-#                 age = age, 
-#                 agent = agent
-#             )
-#             print("leads has been created")
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+#             lead.save()
 #             return redirect("/leads")
             
 #     context = {
-#         "form": form
+#         "form": form,
+#         "lead": lead    
 #     }
+#     return render(request, "leads/lead_update.html", context)
+
+
+# def lead_delete(request, pk):
+#     return render(request, )
+
+# def lead_create(request):
+    # form =LeadModelForm()
+    # if (request.method == "POST"):
+    #     form = LeadModelForm(request.POST)
+    #     if form.is_valid():
+    #         print("form is valid")
+    #         print(form.cleaned_data)
+    #         first_name = form.cleaned_data["first_name"]
+    #         last_name = form.cleaned_data["last_name"]
+    #         age = form.cleaned_data["age"]
+    #         agent = Agent.objects.first()
+    #         Leads.objects.create(
+    #             first_name = first_name, 
+    #             last_name = last_name, 
+    #             age = age, 
+    #             agent = agent
+    #         )
+    #         print("leads has been created")
+    #         return redirect("/leads")
+            
+    # context = {
+    #     "form": form
+    # }
 #     return render(request, "leads/lead_create.html", context)
