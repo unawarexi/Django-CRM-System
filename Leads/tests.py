b43cc8bdd1c707d017bd474c010a43ec0f091716
unawarexi <unawarexi@gmail.com>
2025-01-12 14:34:22 +0100

    writting tests

--- Leads/tests.py
+++ Leads/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from django.shortcuts import reverse
 # Create your tests here.
+class HomePageTest(TestCase):
+     def test_status_codes(self):
+         #TODO some sort of test
+         response = self.client.get(reverse("landing_page"))
+         self.assertEqual(response.status_code, 200)
+         self.assertTemplateUsed(response, "Layout/landing_page.html")
+        
